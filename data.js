00a5fb14aa2d4c84022b83d2cb691ee1a590c032
Student <imshiny808@gmail.com>
2018-09-17 14:19:44 -1000

    oof

---
+++ data.js
@@ -0,0 +1,43 @@
+const data = [
+    {
+        artist: "James Franko",
+        genre: "Regae",
+        song: "Honkey Dory",
+        image: ""
+    },
+    {
+        artist: "Logan Moniz",
+        genre: "ASMR",
+        song: "Cooking with Logan"
+    },
+    {
+      artist: "Kelii Mccary",
+      genre: "Rock",
+      song: "I ain't vegan",
+      image: ""  
+    },
+    {
+        artist: "Kelii Mccary",
+        genre:  "Diss Track",
+        song: "Lary the Fairy",
+        image: ""
+    },
+    {
+        artist: "Jeraldy",
+        genre: "Diss Track",
+        song: "Slim Tim",
+        image: " "
+    },
+    {
+        artist: "Tomato",
+        genre: "Diss Track",
+        song: "Kelii the Vegan",
+        image: ""
+    },
+    {
+        artist: "Kelii",
+        genre: "Diss Track",
+        song: "Keaka the Mexican",
+        image: "" 
+    }
+]
